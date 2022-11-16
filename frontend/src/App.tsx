e971ea6d51c96bc001dc7cbfd573a6cd3620f6b5
thiagobarros00 <thiagosbarros_@hotmail.com>
2022-11-15 23:13:42 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import NotificationButton from "./components/NotificationsButton"
+
 function App() {
   return(
-    <h1>
-      OLÁ MUNDO!
-    </h1>
+    <>
+    <h1>OLÁ MUNDO!</h1>
+    <NotificationButton/>
+    </>
   ) 
 }
 
